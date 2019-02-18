dcc12a28e75d568103675584a7e322ba4a0f06ba
blancafs <43822933+blancafs@users.noreply.github.com>
2019-02-18 13:24:42 +0000

    Final Files
    if timeout = 5ms-25ms image corrupted

--- Receiver1b.java
+++ Receiver1b.java
@@ -3,18 +3,21 @@ import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.net.DatagramSocket;
 import java.util.ArrayList;
+import java.net.DatagramPacket;
+import java.net.SocketAddress;
+import java.util.Arrays;
 
 
 public class Receiver1b {
-	
+
 	private ReceiverState receiverState;
-	
+
 	int port;
 	private File file;
 	private OutputStream os;
 	private DatagramSocket socket;
-	
-	
+
+
 	public Receiver1b(int port, String filename) throws Exception {
 		// Both sender and receiver will start at state 0 (awaiting/sending packages with sequence number of 0)
 		receiverState = new ReceiverState0();
@@ -22,48 +25,46 @@ public class Receiver1b {
 		file = new File(filename);
 		os = new FileOutputStream(file);
 	}
-	
+
 	public void receive() throws Exception {
 		socket = new DatagramSocket(port);
 		ArrayList<byte[]> recPackets = new ArrayList<byte[]>();
-		System.out.println("Socket open.");
-		
+
 		// Until eof with a 1 is found, will keep receiving packages.
-		// The if loop within makes sure no duplicates are added to the final arraylist.
 		while(!receiverState.isFinished()) {
 			byte[] currpacket = receiverState.getPacket(this,socket);
-			System.out.println("Actually received package in Receiver1b!");
 			recPackets.add(currpacket);
 		}
+		// Close socket when done
 		socket.close();
-		
+
 		// Create final image byte array and write to file
 		byte[] tofile = arrify(recPackets);
 		os.write(tofile);
-		System.out.println("Writing to final file ...");
 		os.close();
 		return;
 	}
 
 	public void changeState(ReceiverState receiverState) {
+		// Changes receiver state from 0 to 1 or vice versa, every time after a correctly received package
 		this.receiverState=receiverState;
 	}
-	
-	// Arrify is a simple method to append every element of each byte array in an Arraylist to one final byte array
+
+	// Arrify turns an arraylist into a single byte array we can write to file
 	public byte[] arrify(ArrayList<byte[]> tosend) {
 		int size = 0;
 		for (int i=0; i<tosend.size(); i++) {
 			size += tosend.get(i).length;
 		}
-		
 		byte[] fin = new byte[size];
 		for (int i=0; i<tosend.size(); i++) {
 			int length = tosend.get(i).length;
 			System.arraycopy(tosend.get(i), 0, fin, i*1024, length);
 		}
-		
-		return fin; 
+		return fin;
 	}
+	
+	// Main
 	public static void main(String[] args) throws Exception, Exception {
 		if (args.length<2) {
 			System.out.println("Incorrect arguments given.");
@@ -72,6 +73,136 @@ public class Receiver1b {
 		// Begin receiving
 		Receiver1b receiver = new Receiver1b(Integer.parseInt(args[0]), args[1]);
 		receiver.receive();
-		System.out.println("All done.");
 	}
+	
+	//////////////////////////////////////
+	///////////// FSM CLASSES ////////////
+	//////////////////////////////////////
+
+	abstract class ReceiverState {
+
+		Boolean done = false;
+
+		public abstract byte[] getPacket(Receiver1b receiver, DatagramSocket socket) throws Exception;
+
+		public Boolean isFinished() {
+			return done;
+		}
+	}
+
+	class ReceiverState0 extends ReceiverState{
+
+		public byte[] getPacket(Receiver1b receiver, DatagramSocket socket) throws Exception {
+
+			DatagramPacket packet = null;	// current packet retrieved
+			boolean lastpacket = false;		// see if last
+			boolean correct = false;
+			final int packetsize = 1027;
+			int datasize = 1024;
+			byte[] returndata= null;       // returndata will hold final data to return to receiver class
+
+			while (!correct) {
+				returndata = new byte[datasize];
+
+				// Receiving packet
+				byte[] empty = new byte[packetsize];
+				packet = new DatagramPacket(empty, empty.length);
+				socket.receive(packet);
+
+				// Interpreting data
+				byte[] fulldata = packet.getData();
+				byte[] seq = Arrays.copyOfRange(fulldata, 0, 2);
+				byte eof = fulldata[2];
+				String eofs = Integer.toBinaryString((eof & 0xFF) + 0x100).substring(1);
+				String seqs = Integer.toBinaryString((seq[1] & 0xFF) + 0x100).substring(1);
+
+				// Sending correct ack back
+				SocketAddress address = packet.getSocketAddress();
+				DatagramPacket ackp = new DatagramPacket(seq, seq.length, address);
+				socket.send(ackp);
+
+				// If wrong sequence number, send ack and keep going, if right number set correct to true to break while loop
+				if (seqs.contains("1")) {
+					continue;
+				} else if (!(seqs.contains("1"))) {
+					correct = true;
+				}
+				// Only correct package possible after above if loop, now simply have to distinguish whether last packet or not
+				// and if last packet, account for different size
+				if (eofs.contains("1")) {
+					lastpacket = true;
+					returndata = Arrays.copyOfRange(fulldata, 3, packet.getLength());
+				} else {
+					returndata = Arrays.copyOfRange(fulldata, 3, packetsize);
+				}
+			}
+
+			if(lastpacket) {
+				this.done = true;
+			}else {
+				receiver.changeState(new ReceiverState1());
+			}
+			// Returns received data as byte array
+			return returndata;
+		}
+
+	}
+	class ReceiverState1 extends ReceiverState{
+
+		public byte[] getPacket(Receiver1b receiver, DatagramSocket socket) throws Exception {
+
+			DatagramPacket packet = null;	// current packet retrieved
+			boolean lastpacket = false;		// see if last
+			boolean correct = false;
+			final int packetsize = 1027;
+			int datasize = 1024;
+			byte[] returndata= null;       // returndata will hold final data to return to receiver class
+
+			while (!correct) {
+				returndata = new byte[datasize];
+
+				// Receiving packet
+				byte[] empty = new byte[packetsize];
+				packet = new DatagramPacket(empty, empty.length);
+				socket.receive(packet);
+
+				// Interpreting data
+				byte[] fulldata = packet.getData();
+				byte[] seq = Arrays.copyOfRange(fulldata, 0, 2);
+				byte eof = fulldata[2];
+				String eofs = Integer.toBinaryString((eof & 0xFF) + 0x100).substring(1);
+				String seqs = Integer.toBinaryString((seq[1] & 0xFF) + 0x100).substring(1);
+
+				// Sending correct ack back
+				SocketAddress address = packet.getSocketAddress();
+				DatagramPacket ackp = new DatagramPacket(seq, seq.length, address);
+				socket.send(ackp);
+
+				// If wrong sequence number, send ack and keep going, if right number set correct to true to break while loop
+				if (!(seqs.contains("1"))) {
+					continue;
+				} else {
+					correct = true;
+				}
+				// Only correct package possible after above if loop, now simply have to distinguish whether last packet or not
+				// and if last packet, account for different size
+				if (eofs.contains("1")) {
+					lastpacket = true;
+					returndata = Arrays.copyOfRange(fulldata, 3, packet.getLength());
+				} else {
+					returndata = Arrays.copyOfRange(fulldata, 3, packetsize);
+				}
+			}
+
+			if(lastpacket) {
+				this.done = true;
+			}else {
+				receiver.changeState(new ReceiverState0());
+			}
+			// Returns received data as byte array
+			return returndata;
+		}
+
+	}
+
 }
--- Sender1b.java
+++ Sender1b.java
@@ -2,74 +2,87 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.SocketTimeoutException;
+import java.util.Arrays;
+import java.util.Date;
 import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 
 public class Sender1b {
-	
+
 	private SenderState senderState;
 	int datasize = 1024;
 	int packetsize = 1027;
+	private Date start;
+	private Date end;
+	public int retransmissions;
 	private int port;
 	private int timeout;
 	private String serverName;
 	private String filename;
 	private InetAddress address;
-	
-	
+
 
 	public Sender1b(String servername, String serverport, String filename, String timeout) throws Exception {
 		// Communications will always start in sender state 0
 		senderState = new SenderState0();
+		retransmissions = 0;
 		this.serverName = servername;
 		this.address = InetAddress.getByName(serverName);
 		this.port = Integer.parseInt(serverport);
 		this.filename = filename;
 		this.timeout = Integer.parseInt(timeout);
-	
+
 	}
-	
+
 	public Sender1b sendFile() throws Exception {
-		// Creating socket, and setting input timeout
+		// Creating socket, and setting correct timeout
 		DatagramSocket socket = new DatagramSocket();
-		//socket.setSoTimeout(timeout);
-		
-		// Read wanted file and turn into bytes, then packets, and send one by one
+		socket.setSoTimeout(timeout);
+
+		// Read wanted file and turn into bytes
 		File f = new File(this.filename);
 		byte[] filebytes = new byte[(int)f.length()];
 		FileInputStream fis = new FileInputStream(f);
 		fis.read(filebytes);
 		fis.close();
-		
+
 		// Send each packet made with packify
 		ArrayList<DatagramPacket> packetsToSend = packify(filebytes);
+		// Start timer to measure throughput
+		start = new Date();
 		for (DatagramPacket packet : packetsToSend) {
-			System.out.println("Sending packet " + packetsToSend.indexOf(packet)+ " to senderstate method.");
 			senderState.sendPacket(this, packet, socket);
 		}
-		System.out.println("All packets sent!");
+		end = new Date();
+		long time = end.getTime()-start.getTime();
+		double tput = f.length()/time;
+		// Only printed line from Sender1b
+		System.out.println(retransmissions + " " + tput);
 		// Close socket when all packages sent
 		socket.close();
 		
+		// Return sender object
 		return this;
-		
+
 	}
 	
 	public ArrayList<DatagramPacket> packify(byte[] topackify) {
 		ArrayList<byte[]> packets = new ArrayList<byte[]>();
 		ArrayList<DatagramPacket> finalpackets = new ArrayList<DatagramPacket>();
 		
+		// Nos is the number of full packets of size 1027 made, while dif is the length of the last packet
 		int length = topackify.length;
 		int nos = (int) Math.floor(length/datasize);
 		int dif = length - (nos*datasize);
 		byte[] packet = new byte[datasize];
-		
-		// Iterate through number of packages, copying correct amount of data into each one,
+
+		// Loop iterates through the number of packages, copying the correct amount of data into each one,
 		// taking particular care for the last packet which can have a different size
 		for (int i=0; i<nos+1; i++) {
-			if (i==nos) {
+			if (i==nos) { // when i==nos, we are packing the last packet
 				packet = new byte[dif];
 				packet = Arrays.copyOfRange(topackify, nos*datasize, length);
 				packets.add(packet);
@@ -79,25 +92,25 @@ public class Sender1b {
 			packets.add(packet);
 			packet = new byte[datasize];
 		}
-		
+
 		// For each 1024 byte packet to send, we add the correct sequence number and eof
 		for (byte[] packeta : packets) {
 			byte[] finalpacket = new byte[packeta.length+3];
 			byte[] eof = new byte[1];
 			byte[] sequenceno = new byte[2];
-			if (packets.indexOf(packeta)==packets.size()-1) {
+			if (packets.indexOf(packeta)==packets.size()-1) { // If its the last packet
 				eof[0] |= (1<<0);
 			}
-			if (packets.indexOf(packeta)%2==1) {
+			if (packets.indexOf(packeta)%2==1) { // Packets should be sequenced 0,1,0,1 etc starting with 0
 				sequenceno[1] |= (1<<0);
 			}
-			for (int i=3; i<finalpacket.length; i++) {
+			for (int i=3; i<finalpacket.length; i++) { // The rest of the packet is the data
 				finalpacket[i] = packeta[i-3];
 			}
 			finalpacket[0] = sequenceno[0];
 			finalpacket[1] = sequenceno[1];
 			finalpacket[2] = eof[0];
-			
+
 			// We add the finalised datagram packet to the final arraylist
 			DatagramPacket fpacket = new DatagramPacket(finalpacket, finalpacket.length, address, port);
 			finalpackets.add(fpacket);
@@ -106,33 +119,140 @@ public class Sender1b {
 	}
 
 	public void changeState(SenderState senderstate) {
+		// Used when a package is sent correctly
 		this.senderState=senderstate;
-		
 	}
-	
-	// Arrify is a simple method to append every element of each byte array in an Arraylist to one final byte array
-	public byte[] arrify(ArrayList<byte[]> tosend) {
-		int size = 0;
-		for (int i=0; i<tosend.size(); i++) {
-			size += tosend.get(i).length;
-		}
-		
-		byte[] fin = new byte[size];
-		for (int i=0; i<tosend.size(); i++) {
-			int length = tosend.get(i).length;
-			System.arraycopy(tosend.get(i), 0, fin, i*1024, length);
-		}
-		
-		return fin; 
-	}
-	
+
 	// Main
 	public static void main(String[] args) throws Exception, Exception {
 		if (args.length<2) {
+			// When wrong arguments given, simple error message
 			System.out.println("Incorrect arguments given.");
 			return;
 		}
 		Sender1b newsender = new Sender1b(args[0],args[1], args[2], args[3]).sendFile();
-		System.out.println("Sent all packets successfully.");
 	}
+	
+	//////////////////////////////////////
+	// ////////// FSM CLASSES ////////////
+	//////////////////////////////////////
+	
+	
+	public abstract class SenderState {
+
+		boolean done = false;
+
+		public abstract void sendPacket(Sender1b sender, DatagramPacket packet, DatagramSocket socket) throws Exception;
+
+	}
+	class SenderState0 extends SenderState{
+
+		@Override
+		public void sendPacket(Sender1b sender, DatagramPacket packet, DatagramSocket socket) throws Exception {
+
+			boolean sending = true;
+			boolean last = true;
+			int lasttries = 0;
+			DatagramPacket emptyack = null;
+			int packetsize = 1027;
+			int datasize = 1024;
+			int acksize = 2;
+
+			while (sending) {
+				// If fails to receive ack 10 times, we end loop and assume it was the last one and ack got lost but receiver finished
+				if (lasttries==10) {
+					return;
+				}
+				
+				socket.send(packet); 
+
+				// To know if last packet
+				byte[] fulldata = packet.getData();
+				byte eof = fulldata[2];
+				String eofs = Integer.toBinaryString((eof & 0xFF) + 0x100).substring(1);
+				
+				// Receive ack, catching the timeout exception
+				byte[] empty2 = new byte[acksize];
+				emptyack = new DatagramPacket(empty2, empty2.length);
+				try {
+					socket.receive(emptyack);
+				} catch (SocketTimeoutException e) {
+					if (eofs.contains("1")) {
+						lasttries++; // If last packet, and timeout occurs, only let it happen 10 times
+					}
+					sender.retransmissions++;
+					continue; // Timeout reached, resend packet!
+				}
+
+				// Verify ack received
+				byte[] fulldata2 = emptyack.getData();
+				byte seq2 = fulldata2[1];
+				String seqs2 = Integer.toBinaryString((seq2 & 0xFF) + 0x100).substring(1);
+				if (!(seqs2.contains("1"))) {  // If no 1, correct ack received in senderstate0, we end while loop and move on to state 1
+					sending = false;
+					sender.changeState(new SenderState1());
+				} else if (seqs2.contains("1")) { // If incorrect ack, resend
+					sender.retransmissions++;
+					continue;
+				}
+				// If sending = true, the ack was not right, and so packet is sent again
+			}
+			return;
+		}
+	}
+	class SenderState1 extends SenderState{
+		
+
+		@Override
+		public void sendPacket(Sender1b sender, DatagramPacket packet, DatagramSocket socket) throws Exception {
+
+			boolean sending = true;
+			DatagramPacket emptyack = null;
+			int packetsize = 1027;
+			int datasize = 1024;
+			int acksize = 2;
+			int lasttries = 0;
+
+			while (sending) {
+				// If fails to receive ack 10 times, we end loop and assume it was the last one and ack got lost but receiver finished
+				if (lasttries==10) {
+					return;
+				}
+				socket.send(packet); // Timer started
+
+				// To know if last packet
+				byte[] fulldata = packet.getData();
+				byte eof = fulldata[2];
+				String eofs = Integer.toBinaryString((eof & 0xFF) + 0x100).substring(1);
+				
+				// Receive ack, catching the timeout exception
+				byte[] empty2 = new byte[acksize];
+				emptyack = new DatagramPacket(empty2, empty2.length);
+				try {
+					socket.receive(emptyack);
+				} catch (SocketTimeoutException e) {
+					if (eofs.contains("1")) {
+						lasttries++; // If last packet, and timeout occurs, only let it happen 10 times
+					}
+					sender.retransmissions++;
+					continue; // Resend packet!
+				}
+
+				// Verify ack
+				byte[] fulldata2 = emptyack.getData();
+				byte seq2 = fulldata[1];
+				String seqs2 = Integer.toBinaryString((seq2 & 0xFF) + 0x100).substring(1);
+				if (seqs2.contains("1")) { // If 1 is in sq, correct ack received in senderstate1, we end while loop and move on to state 0
+					sending = false;
+					sender.changeState(new SenderState0());
+				} else if (!(seqs2.contains("1"))) { // If incorrect ack, resend
+					sender.retransmissions++;
+					continue; 
+				}
+				// If sending = true, the ack was not right, and so packet is sent again
+			}
+			return;
+		}
+	}
+
 }
